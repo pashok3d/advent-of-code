9e15eb5811ea4f3cd205b4a99faf03518ffae2bf
pashok3d <pavel.tarashkevich@gmail.com>
2024-12-20 10:51:04 +0100

    Upload 7th day part 2

---
+++ 2024/7/7_2.py
@@ -0,0 +1,48 @@
+"""
+1. Read input
+2. For each equation:
+    2.1 generate all combinations of operators
+    2.2 evaluate the result
+"""
+
+from itertools import product
+
+OPERATORS = ["+", "*", "||"]
+
+with open("2024/7/input7.txt", "r") as f:
+    lines = f.readlines()
+
+sum = 0
+
+
+def evaluate(params: list[int], combination: tuple[str], result: int):
+    if combination[0] == "+":
+        eval_result = params[0] + params[1]
+    elif combination[0] == "||":
+        eval_result = int(str(params[0]) + str(params[1]))
+    else:
+        eval_result = params[0] * params[1]
+
+    for param, operator in zip(params[2:], combination[1:]):
+        if operator == "+":
+            eval_result += param
+        elif operator == "||":
+            eval_result = int(str(eval_result) + str(param))
+        else:
+            eval_result *= param
+
+        if eval_result > result:
+            break
+
+    return eval_result == result
+
+
+for line in lines:
+    result = int(line.split(":")[0])
+    params = list(map(int, line.split(":")[1].split()))
+    for combination in product(OPERATORS, repeat=len(params) - 1):
+        if evaluate(params, combination, result):
+            sum += result
+            break
+
+print(sum)
